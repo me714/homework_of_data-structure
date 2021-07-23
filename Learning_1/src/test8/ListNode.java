ef54fc39cd17c796ffd707907b27e81086254766
me714 <73265048+me714@users.noreply.github.com>
2021-07-23 21:02:38 +0800

    第八次作业

---
+++ Learning_1/src/test8/ListNode.java
@@ -0,0 +1,29 @@
+package test8;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    public ListNode(int val){
+        this.val = val;
+    }
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        ListNode dummyHead = new ListNode(-1,this);
+        ListNode cur = dummyHead.next;
+        while (cur !=null){
+            res.append(cur.val).append("->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+}
+
+
